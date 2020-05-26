51800b1cf5f6590f6ddc10590bcc93eb1b1f6b0d
Xelor <lucianodegni@gmail.com>
2020-05-26 10:06:53 +0200

    update hello

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "THIS IS AN HELLO WORLD FOR BE"
 
 if __name__ == "__main__":
     application.run()
