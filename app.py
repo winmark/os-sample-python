541f5a397c6c5e6da6991e79cb750f710877bf21
Luciano <lucianodegni@gmail.com>
2020-05-27 09:24:12 +0200

    updated file and config

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    application.run()
