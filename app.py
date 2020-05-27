426cc678ca33d05be29226ac84a6e9a96780003a
Xelor <lucianodegni@gmail.com>
2020-05-27 15:30:44 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    application.run()
